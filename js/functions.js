d0a0097e7b058b32e47b7a7ed6f4279cedb49cfd
dbsimeonov <dbsimeonov@yahoo.com>
2018-04-08 12:34:53 +0100

    created modalPop by css class

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,35 @@
 //  Functions
 
-const modalAbout = e =>{
+// const modalAbout = e =>{
 	
+// 	const target  = document.querySelector('#aboutme');
+// 	const content = document.querySelector('.header__content');
+
+// 	if(target.style.visibility == 'visible'){
+// 		target.style.opacity = '0';
+// 		target.style.visibility = 'hidden';
+// 		// content.style.opacity = '1';
+// 	}else{
+// 		target.style.opacity = '1';
+// 		target.style.visibility = 'visible';
+// 		// content.style.opacity = '0';
+// 	}
+// 	// console.log('Event has triggered');
+// }
+
+const modalAbout = () => {
+	// Declaring variables about the modal
 	const target  = document.querySelector('#aboutme');
 	const content = document.querySelector('.header__content');
 
-	if(target.style.visibility == 'visible'){
-		target.style.opacity = '0';
-		target.style.visibility = 'hidden';
-		// content.style.opacity = '1';
+	if(target.classList.contains('modalPop')){
+		target.classList.remove('modalPop');
 	}else{
-		target.style.opacity = '1';
-		target.style.visibility = 'visible';
-		// content.style.opacity = '0';
+		target.classList.add('modalPop');
 	}
-	
 
-	// console.log('Event has triggered');
 
 }
-
-
 
 const goHome = e => {
 	const home 		= document.querySelector('#homePage');
@@ -51,3 +60,14 @@ const projects = e => {
 	projects.style.opacity = '1';
 
 }
+
+
+// Create the animation with css classes
+// - Onload add class for home page sliding text
+// - Onclick remove class and add class for fade-left/right
+// - after that function for adding class from left/right
+// - each calss needs to have transform/visibility/opacity
+// Modal -
+// Do it with toggling classes
+
+
