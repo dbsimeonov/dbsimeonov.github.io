a9856618a0993771896c51145ac2f94767a900b7
dbsimeonov <dbsimeonov@yahoo.com>
2018-04-09 10:52:48 +0100

    added cursor speed js

--- js/functions.js
+++ js/functions.js
@@ -51,11 +51,64 @@ const projects = e => {
 // - each calss needs to have transform/visibility/opacity
 // Modal -
 // Do it with toggling classes
+
+let csCalculator = () => {
+	let lastmousex = -1;
+	let lastmousey = -1;
+	let prevCallTime = -1;
+	let mousetravel = 0;
+	let target = document.querySelector('#cSpeed');
+	var lastmousetime;
+	var mrefreshinterval = 500;
+
+	let handleMouseMove = e => {
+		let calledAt = Date.now();
+
+		const mousex = e.pageX;
+		const mousey = e.pageY;
+
+		if (lastmousex !== -1 || lastmousey !== -1) {
+			mousetravel += Math.max(
+			  Math.abs(mousex - lastmousex),
+			  Math.abs(mousey - lastmousey)
+			);
+		}
+		lastmousex = mousex;
+		lastmousey = mousey;
+
+		setTimeout(reportSpeed, mrefreshinterval);
+	};
+
+	const reportSpeed = function() {
+		var now = new Date();
+		var timenow = now.getTime();
+
+		if (lastmousetime && lastmousetime !== timenow && mousetravel !== 0) {
+			let pps = Math.round(mousetravel / (timenow - lastmousetime) * 1000 );
+			mousetravel = 0;
+
+			function circumference(r) {
+			  return parseFloat(r) * 0.001 * Math.PI;
+			}
+		// If you don't want to report the mouse standing still repeatedly
+			// pps !== 0 &&
+			document.getElementById('parameters').innerHTML = circumference(pps);
+			}
+		lastmousetime = timenow;
+	};
+
+	document.onmousemove = handleMouseMove;
+
+	document.onmousemove = handleMouseMove;
+}
+
 window.onload = () => {
 	// add class for loading page(homePage )
 	const homePage = document.getElementById('homePage');
 
 	homePage.classList.add('scalePop');
+
+	csCalculator();
 
 }
 
